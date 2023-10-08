f24425d00811e4830cac9419d517a86bd7cdaa62
Lucas Knupp <lucas_knupp@outlook.com>
2023-10-08 10:32:04 -0300

    API para a chamada da IA

--- api/api.py
+++ api/api.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from iaTraining import fazer_previsao
 from beautifulSoap import extract_elements_for_anchor
+from joblib import load
 
 app = Flask(__name__)
 
@@ -14,6 +16,29 @@ def get_data(anchor):
         return jsonify(elements)
     else:
         return jsonify({'error': 'Failed to retrieve data for anchor'}), 500
+
+
+@app.route('/api/consultar', methods=['POST'])
+def consultar():
+    modelo_categoria = load('modelo_categoria.joblib')
+    modelo_agencia = load('modelo_agencia.joblib')
+    modelo_topico = load('modelo_topico.joblib')
+    vetorizador = load('vetorizador.joblib')
+
+    consulta = request.json.get('consulta')
+
+    if consulta is None:
+        return jsonify({'error': 'Consulta ausente'}), 400
+
+    categoria_predita, agencia_predita, topico_predito = fazer_previsao(consulta)
+
+    resposta = {
+        'categoria_predita': categoria_predita[0],
+        'agencia_predita': agencia_predita[0],
+        'topico_predito': topico_predito[0]
+    }
+    return jsonify(resposta)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
